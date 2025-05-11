4367b83fd5690739948a8fe727b8f5b86680d4ca
Egor Tokarev <egortok2014@gmail.com>
2025-05-12 00:35:14 +0300

    Add seter and geter for Property

--- Property.h
+++ Property.h
@@ -7,5 +7,8 @@ public:
 	Property(unsigned int worth);
 	virtual double ñalculationTax() = 0;
 	double calculationIncomeTax();
+
+	inline unsigned int getWorth() { return worth; };
+	inline void setWorth(unsigned int value) { worth = value; };
 };
 
