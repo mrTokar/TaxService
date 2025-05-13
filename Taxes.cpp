7c27183a01c5008f79da11dd8a129ccc85cade62
Egor Tokarev <egortok2014@gmail.com>
2025-05-13 23:20:36 +0300

    Change main

--- Taxes.cpp
+++ Taxes.cpp
@@ -1,28 +1,21 @@
 ﻿#include <iostream>
-#include <Windows.h>
-#include "Owner.h"
-#include "Apartament.h"
-#include "Car.h"
-#include "CountryHouse.h"
-#include "UI.h"
+#include "TaxService.h"
 using namespace std;
 
-int main() {
-	setlocale(LC_ALL, "ru");
-
-	auto owner = readjson();
-
-	if (owner.getFullname() == "") {
-		return 0;
+int main(int argc, char* argv[]) {
+	
+	if (argc == 3) {
+		try {
+		TaxService ts(argv[1]);
+		ts.calculate_and_save(argv[2]);
+		cout << "Successful compete" << endl;
+		}
+		catch (...){
+			cout << "Run time error" << endl;
+		}
 	}
-
-	nlohmann::json json= owner.toJson();
-	writejson(json);
-
-	// clear mem
-	for (auto prop : owner.getProperties())
-	{
-		delete prop;
+	else {
+		clog << "ERROR: 2 parameters expected" << endl;
 	}
 
 	return 0;
