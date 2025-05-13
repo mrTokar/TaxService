2059f662817f7a743075626e551924d3814d5f0a
Egor Tokarev <egortok2014@gmail.com>
2025-05-13 22:08:14 +0300

    Add TaxService::fromJson()

--- TaxService.cpp
+++ TaxService.cpp
@@ -1,5 +1,39 @@
 #include "TaxService.h"
 
+void TaxService::fromJson(string const& filename)
+{
+	ifstream fin(filename);
+	if (!fin.is_open()) {
+		clog << "ERROR: File " << filename << " don't exist" << endl;
+		throw runtime_error("File don't exist");
+	}
+
+	nlohmann::json json_file = nlohmann::json::parse(fin);
+	fin.close();
+	
+	if (json_file.contains("owners")) 
+	{
+		auto json_owners = json_file["owners"].get<vector<nlohmann::json>>();
+
+		Owner owner("", "", {});
+		for (auto& json_owner : json_owners)
+		{
+			try {
+				owner.fromJson(json_owner);
+				owners.push_back(owner);
+			}
+			catch (exception e) {
+				clog << "ERROR: " << e.what() << endl;
+				throw e;
+			}
+		}
+	}
+	else {
+		clog << "No key: 'owners'" << endl;
+		throw exception("No key: 'owners'");
+	}
+}
+
 TaxService::~TaxService()
 {
 	for (auto& owner : owners)
@@ -10,3 +44,4 @@ TaxService::~TaxService()
 		}
 	}
 }
+
--- TaxService.h
+++ TaxService.h
@@ -1,12 +1,17 @@
 #pragma once
 #include "Owner.h"
+#include <fstream>
+#include <iostream>
 using namespace std;
 
 class TaxService
 {
 	vector<Owner> owners;
+	
+	void fromJson(string const& filename);
 public:
-	TaxService() {};
+	TaxService(string const& input) {};
 	~TaxService();
+
 };
 
