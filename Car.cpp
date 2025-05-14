7e9b73e90599df0dfb2c7984881a18aed2b687f1
Egor Tokarev <egortok2014@gmail.com>
2025-05-14 11:13:56 +0300

    Add xmlio to Car

--- Car.cpp
+++ Car.cpp
@@ -1,6 +1,7 @@
 #include "Car.h"
 #include "TaxRate.h"
 using namespace TAX_RATES;
+using namespace pugi;
 
 Car::Car(int worth, float horsepower): Property(worth)
 {
@@ -29,4 +30,28 @@ void Car::fromJson(nlohmann::json json)
 	catch (...) {
 		throw std::runtime_error("No required keys in json");
 	}
+}
+
+void Car::fromXml(pugi::xml_node& node)
+{
+	try {
+		horsepower = node.attribute("horsepower").as_double();
+		worth = node.attribute("worth").as_double();
+	}
+	catch (...) {
+		throw std::runtime_error("No required attribute in xml");
+	}
+}
+
+void Car::toXml(xml_node& root)
+{
+	xml_node apartament = root.append_child("Car");
+
+	xml_attribute square_attr = apartament.append_attribute("horsepower");
+	square_attr.set_value(horsepower);
+
+	xml_attribute worth_attr = apartament.append_attribute("worth");
+	worth_attr.set_value(worth);
+
+	apartament.text().set(ñalculationTax());
 }
