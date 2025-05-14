9e4c4b57a736f1ebfb89c10d6f0c65bf40635447
Egor Tokarev <egortok2014@gmail.com>
2025-05-14 11:17:07 +0300

    Add xmlio for CountryHouse

--- CountryHouse.h
+++ CountryHouse.h
@@ -9,6 +9,10 @@ public:
     CountryHouse(int worth, unsigned int distanceFromCity);
     inline ~CountryHouse() override {};
     double ñalculationTax() override;
+
     void fromJson(nlohmann::json json) override;
+
+    void fromXml(pugi::xml_node& node) override;
+    void toXml(pugi::xml_node& root) override;
 };
 
