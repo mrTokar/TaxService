fdd6c5e9843acbe10c41fe73b7d8dd29a0279008
Egor Tokarev <egortok2014@gmail.com>
2025-05-14 10:52:26 +0300

    Add root param

--- Ixmlio.h
+++ Ixmlio.h
@@ -5,7 +5,7 @@ class Ixmlio
 {
 public:
 	virtual void fromXml(pugi::xml_node node) = 0;
-	virtual pugi::xml_node toXml() = 0;
+	virtual pugi::xml_node toXml(pugi::xml_node& root) = 0;
 	virtual ~Ixmlio() {};
 };
 
