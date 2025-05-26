201b3de0d361edf084663066d0c9c20194c4ad39
Egor Tokarev <egortok2014@gmail.com>
2025-05-26 12:38:59 +0300

    Fix returned object

--- TaxService/Property/PropertyFactory.h
+++ TaxService/Property/PropertyFactory.h
@@ -8,6 +8,6 @@ class PropertyFactory
 	PropertyFactory() {};
 public:
 	template <class T, typename = std::enable_if_t<std::is_base_of<Property, T>::value>>
-	static T* create() { return T(); }
+	static T* create() { return new T(); }
 };
 
